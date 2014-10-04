5f4eb9b232294d2bce44e4b590a650c1ace2c132
bulton-fr <bulton.fr@gmail.com>
2014-10-04 21:00:00 +0200

    Add unit test

---
+++ test/classes/Template.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Fichier de test pour une class
+ */
+
+namespace BFWTpl\test\unit;
+use \atoum;
+
+require_once(__DIR__.'/../common.php');
+
+/**
+ * Test de la class Template
+ */
+class Template extends atoum
+{
+    /**
+     * @var $class : Instance de la class Template
+     */
+    protected $class;
+
+    /**
+     * @var $mock : Instance du mock pour la class Template
+     */
+    protected $mock;
+
+    /**
+     * Instanciation de la class avant chaque méthode de test
+     */
+    public function beforeTestMethod($testMethod)
+    {
+        //$this->class = new \BFWTpl\Template();
+        //$this->mock  = new MockTemplate();
+    }
+
+    /**
+     * Test de la méthode getBlock()
+     */
+    public function testGetBlock()
+    {
+        
+    }
+
+    /**
+     * Test du constructeur : Template($file, $vars=)
+     */
+    public function testTemplate()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode EndBlock()
+     */
+    public function testEndBlock()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode AddGeneralVars($vars)
+     */
+    public function testAddGeneralVars()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode AddVars($vars, $name=)
+     */
+    public function testAddVars()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode remonte()
+     */
+    public function testRemonte()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode AddBlockWithEnd($name, $varsOrEnd=, $end=)
+     */
+    public function testAddBlockWithEnd()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode AddBlock($name, $vars=, $end=)
+     */
+    public function testAddBlock()
+    {
+        
+    }
+
+    /**
+     * Test de la méthode End($no_echo=0)
+     */
+    public function testEnd()
+    {
+        
+    }
+
+}
+
+/**
+ * Mock pour la classe Template
+ */
+class MockTemplate extends \BFWTpl\Template
+{
+    /**
+     * Accesseur get
+     */
+    public function __get($name) {return $this->$name;}
+}
